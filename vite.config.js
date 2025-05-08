ecde8a11626d5b2e10c47b7deadce5828f605235
Dark <djones.sdx@gmail.com>
2025-05-08 18:23:38 +0600

    port change 3000 to 4000

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 4000, // Changed to port 4000
     host: true,
     strictPort: true,
-  },
-});
+  }
+})
