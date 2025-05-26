e9569fa01519cfa69a949e22afc4872a80277820
Dark <djones.sdx@gmail.com>
2025-05-26 22:57:52 +0600

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rabbana-40-Dua-Collection/',
+  base: './', // Change this line to use relative paths
   server: {
     port: 4000,
     host: true,
     strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
   },
 });
